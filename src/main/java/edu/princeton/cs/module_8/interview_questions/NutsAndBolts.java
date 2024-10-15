3b4b4a51dbe14e3e8d769ea209ae707b59e5d2a6
Matteo <matteo.campana996@gmail.com>
2024-10-15 17:38:01 +0200

    module 8

---
+++ src/main/java/edu/princeton/cs/module_8/interview_questions/NutsAndBolts.java
@@ -0,0 +1,77 @@
+package edu.princeton.cs.module_8.interview_questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class NutsAndBolts {
+
+    // Main method to test the algorithm
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        // Sample nuts and bolts represented by integers for simplicity
+        ArrayList<Integer> nuts = new ArrayList<Integer>();
+        ArrayList<Integer> bolts = new ArrayList<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            nuts.add(i);
+            bolts.add(i);
+        }
+
+        Collections.shuffle(nuts);
+        Collections.shuffle(bolts);
+
+        System.out.println("Unsorted nuts:\t" + nuts);
+        System.out.println("Unsorted bolts:\t" + bolts);
+
+        // Perform matching
+        matchPairs(nuts, bolts, 0, nuts.size() - 1);
+
+        // Display matched nuts and bolts
+        System.out.println("=".repeat(50));
+        System.out.println("Matched nuts:\t" + nuts);
+        System.out.println("Matched bolts:\t" + bolts);
+    }
+
+    // Function to match pairs of nuts and bolts
+    static void matchPairs(ArrayList<Integer> nuts, ArrayList<Integer> bolts, int low, int high) {
+        if (low < high) {
+            // Choose the first nut as the pivot for bolts partitioning
+            int pivotIndex = partition(bolts, low, high, nuts.get(low));
+
+            // Use the corresponding bolt to partition the nuts
+            partition(nuts, low, high, bolts.get(pivotIndex));
+
+            // Recursively match the left and right partitions
+            matchPairs(nuts, bolts, low, pivotIndex - 1);
+            matchPairs(nuts, bolts, pivotIndex + 1, high);
+        }
+    }
+
+    // Partition the array such that elements smaller than the pivot come before
+    // the pivot and elements greater than the pivot come after it.
+    static int partition(ArrayList<Integer> arr, int low, int high, Integer pivot) {
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (arr.get(j) < pivot) {
+                swap(arr, i, j);
+                i++;
+            } else if (arr.get(j).equals(pivot)) {
+                swap(arr, j, high);  // Move pivot element to the end
+                j--;
+            }
+        }
+        swap(arr, i, high);  // Move pivot to its final position
+        return i;
+    }
+
+    // Utility function to swap elements
+    static void swap(ArrayList<Integer> arr, int i, int j) {
+        Integer temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+}
---
+++ src/main/java/edu/princeton/cs/module_8/interview_questions/SelectionInTwoSortedArrays.java
@@ -0,0 +1,113 @@
+package edu.princeton.cs.module_8.interview_questions;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SelectionInTwoSortedArrays {
+
+
+    private static void version1() {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        Random rnd = new Random();
+
+        int n = 50;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                a.add(i);
+            } else {
+                b.add(i);
+            }
+        }
+
+        assert a.size() == b.size();
+
+        int k = findKthVersion(a, b, 0, 0, a.size() - 1, b.size() - 1, a.size() + b.size() / 2);
+        System.out.println("a[] : " + a);
+        System.out.println("b[] : " + b);
+        System.out.println("Version 1 output: " + k);
+    }
+
+    private static Integer findKthVersion(ArrayList<Integer> a, ArrayList<Integer> b, int startA, int startB, int endA, int endB, int k) {
+
+        if (startA > endA) return a.get(startA + k);
+        if (startB > endB) return b.get(startB + k);
+
+        int midA = startA + (endA - startA) / 2;
+        int midB = startB + (endB - startB) / 2;
+
+        if (midA + midB < k) {
+            if (a.get(midA) < b.get(midB)) {
+                return findKthVersion(a, b, startA, startB + 1, endA, endB, k - (midA - startA + 1));
+            } else {
+                return findKthVersion(a, b, startA, startB + 1, endA, endB, k - (midB - startB + 1));
+            }
+
+        } else {
+            if (a.get(midA) < b.get(midB)) {
+                return findKthVersion(a, b, startA, startB, endA, midB - 1, k);
+            } else {
+                return findKthVersion(a, b, startA, startB, midA - 1, endB, k);
+            }
+        }
+
+    }
+
+    private static void version2() {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        Random rnd = new Random();
+
+        int n = 50;
+
+        for (int i = 0; i < n; i++) {
+            if (rnd.nextBoolean()) {
+                a.add(i);
+            } else {
+                b.add(i);
+            }
+        }
+        int kth;
+        kth = findKthVersion(a, b, 0, 0, a.size() - 1, b.size() - 1, a.size() + b.size() / 2);
+
+
+        System.out.println("a[] : " + a);
+        System.out.println("b[] : " + b);
+        System.out.println("Version 2 output: " + kth);
+    }
+
+    private static void version3() {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        Random rnd = new Random();
+
+        int n = 50;
+
+        for (int i = 0; i < n; i++) {
+            if (rnd.nextBoolean()) {
+                a.add(i);
+            } else {
+                b.add(i);
+            }
+        }
+        int k, kth;
+        k = rnd.nextInt(a.size() + b.size() - 1);
+        kth = findKthVersion(a, b, 0, 0, a.size() - 1, b.size() - 1, k);
+
+
+        System.out.println("a[] : " + a);
+        System.out.println("b[] : " + b);
+        System.out.println("k :   " + k);
+        System.out.println("Version 3 output: " + kth);
+    }
+
+
+    public static void main(String[] args) {
+        version1();
+        System.out.println("=".repeat(50));
+        version2();
+        System.out.println("=".repeat(50));
+        version3();
+    }
+}
